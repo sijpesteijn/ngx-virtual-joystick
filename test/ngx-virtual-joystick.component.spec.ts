5f557a1aa929109de44ebc2aeccdbd6d60bc8f4e
gijs.sijpesteijn <gijs@jpoint.nl>
2017-05-14 12:39:34 +0200

    Nothing really

--- test/ngx-virtual-joystick.component.spec.ts
+++ test/ngx-virtual-joystick.component.spec.ts
@@ -7,7 +7,7 @@ import {
 import { NgxVirtualJoystickModule } from '../index';
 import { NgxVirtualJoystickComponent } from '../src/ngx-virtual-joystick.component';
 
-xdescribe('virtual-joystick component', () => {
+describe('virtual-joystick component', () => {
     let fixture: ComponentFixture<NgxVirtualJoystickComponent>;
     let component: NgxVirtualJoystickComponent;
     let element: HTMLElement;
@@ -26,34 +26,7 @@ xdescribe('virtual-joystick component', () => {
 
     it('should show default', () => {
         this.fixture.detectChanges();
-        expect(this.component.question).toEqual('Are you sure?');
         expect(this.component.show).toBe(false);
-        expect(this.element.querySelectorAll('.fa-trash').length).toBe(1);
-        expect(this.element.querySelectorAll('.delete-confirm-question').length).toBe(0);
-    });
-
-    it('should show the question when clicked', () => {
-        this.component.show = true;
-        this.fixture.detectChanges();
-        expect(this.component.show).toBe(true);
-        expect(this.element.querySelectorAll('.fa-trash').length).toBe(1);
-        expect(this.element.querySelectorAll('.delete-confirm-question').length).toBe(1);
-    });
-
-    it('should confirm the delete when ok clicked', () => {
-        this.component.show = true;
-        spyOn(this.component.onConfirm, 'emit');
-        this.fixture.detectChanges();
-        this.component.sendConfirm();
-        expect(this.component.onConfirm.emit).toHaveBeenCalled();
-    });
-
-    it('should confirm the cancel when cancel clicked', () => {
-        this.component.show = true;
-        spyOn(this.component.onCancel, 'emit');
-        this.fixture.detectChanges();
-        this.component.sendCancel();
-        expect(this.component.onCancel.emit).toHaveBeenCalled();
     });
 
 });
